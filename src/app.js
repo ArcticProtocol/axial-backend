641568f8ecb586430ab3f4eb8c596fd20276f308
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2023-06-09 21:40:13 +0530

    add example request

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import { Client } from "node-appwrite";
+import { Client, Databases, Users } from "node-appwrite";
+import {config} from 'dotenv';
 
 const app = express();
+config();
 
 // Set up CORS.
 app.use(cors());
@@ -15,17 +17,34 @@ app.use(helmet());
 const appwrtieClient = new Client();
 
 appwrtieClient
-  .setEndpoint(process.env.ENDPOINT) // Your API Endpoint
+  .setEndpoint("https://cloud.appwrite.io/v1") // Your API Endpoint
   .setProject(process.env.PROJECT_ID) // Your project ID
   .setKey(process.env.SECRET_KEY) // Your secret API key
   .setSelfSigned();
 
+const users = new Users(appwrtieClient);
+const database = new Databases(appwrtieClient);
 // Add routes to your app.
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
+app.get("/create", async (req, res) => {
+  try {
+    let promise = await users.create(
+      "asdhaknsa",
+      "email@example.com",
+      undefined,
+      "password",
+      "Jane Doe"
+    );
+    res.send(promise);
+  } catch (error) {
+    console.log({error})
+    res.send({
+      message: "something went wrong",
+    });
+  }
 });
 
+
 // Start the server.
-app.listen(3000, () => {
-  console.log("App listening on port 3000!");
+app.listen(3001, () => {
+  console.log("App listening on port 3001!");
 });
